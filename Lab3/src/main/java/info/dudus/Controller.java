d8b15d552050a91a5236ad861285083864a0eaa2
fOrest94 <sewerdudek15@gmail.com>
2017-03-12 20:24:28 +0100

    third finish

---
+++ Lab3/src/main/java/info/dudus/Controller.java
@@ -0,0 +1,50 @@
+package info.dudus;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Created by f0rest94 on 2017-03-06.
+ */
+@WebServlet("/main")
+public class Controller extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String FILE_PATH = getServletContext().getRealPath("/WEB-INF/");
+        OutputStream out = response.getOutputStream();
+
+        try {
+            int number = Integer.valueOf(request.getParameter("picture"));
+            File file;
+            if (number < 5) {
+                response.setContentType("image/jpeg");
+                file = new File(FILE_PATH + number + ".jpg");
+            } else {
+                response.setContentType("image/gif");
+                file = new File(FILE_PATH + number + ".gif");
+            }
+            FileInputStream in = new FileInputStream(file);
+            byte[] buf = new byte[1024];
+            int count = 0;
+            while ((count = in.read(buf)) >= 0) {
+                out.write(buf, 0, count);
+            }
+            response.setContentLength((int) file.length());
+            out.close();
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Only real numbers are allowed");
+        }
+    }
+}
---
+++ Lab3/src/main/webapp/index.jsp
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="ISO-8859-1">
+    <title>Insert title here</title>
+</head>
+<body>
+<h1>Fill form located below</h1>
+<h3>Homie, what continent you wanna see?</h3>
+<h4>Take your pig</h4>
+<form action="/main" method="post">
+    <input type="radio" name="picture" value="1"> Europa
+    <input type="radio" name="picture" value="2"> North America
+    <input type="radio" name="picture" value="3"> South America
+    <input type="radio" name="picture" value="4"> Asia
+    <input type="radio" name="picture" value="5"> Australia
+    <input type="radio" name="picture" value="6"> Africa
+    <input type="radio" name="picture" value="7"> Antarctica
+    <br>
+    <input type="submit" value="Show me!">
+</form>
+</body>
+</html>
